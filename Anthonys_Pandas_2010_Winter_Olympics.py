cafd8a42800dc3e5deeecd7a28cedd2f6b674bab
ellsberry <sellsberry@sbcglobal.net>
2023-02-18 10:02:18 -0500

    added stars.py

--- Anthonys_Pandas_2010_Winter_Olympics.py
+++ Anthonys_Pandas_2010_Winter_Olympics.py
@@ -14,11 +14,11 @@ if __name__ == "__main__":
     print(olympics_df.head)
     print("----------------------------------------------")
     print(olympics_df.describe(include="all"))
-    temp = olympics_df.iloc[:, 4]
+    # temp = olympics_df.iloc[:, 4]
     # print(temp)
     # print(temp.describe())
-    temp2 = olympics_df.iloc[0:8, 4]
-    print(temp)
-    print("----------------------------------------------")
-    print(temp2.describe())
-    print("----------------------------------------------")
+    # temp2 = olympics_df.iloc[0:8, 4]
+    # print(temp)
+    # print("----------------------------------------------")
+    # print(temp2.describe())
+    # print("----------------------------------------------")
