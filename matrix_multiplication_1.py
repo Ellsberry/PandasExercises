80834067487b488b1ace19bd5e7b397a437cd490
Anthony081111 <ajh081111@gmail.com>
2023-04-15 10:52:50 -0400

    stuff!!!

--- matrix_multiplication_1.py
+++ matrix_multiplication_1.py
@@ -2,7 +2,7 @@
 import numpy as np
 from pprint import pprint
 
-"""
+
 # print(np.array.__dir__())
 # pprint(np.array.__dir__())
 
@@ -12,40 +12,40 @@ from pprint import pprint
 #
 
 a_matrix = np.array([[1, 2, 3, 4, 5, 6],
-               [7, 8, 9, 10, 11, 12],
-               [13, 14, 15, 16, 17, 18],
-               [19, 20, 21, 22, 23, 24],
-               [25, 26, 27, 28, 29, 30],
-               [31, 32, 33, 34, 35, 36]])
-print(f"Line 20 a_matrix \n", a_matrix, "\n", "\n")
+                    [7, 8, 9, 10, 11, 12],
+                    [13, 14, 15, 16, 17, 18],
+                    [19, 20, 21, 22, 23, 24],
+                    [25, 26, 27, 28, 29, 30],
+                    [31, 32, 33, 34, 35, 36]])
+print("Line 20 a_matrix \n", a_matrix, "\n", "\n")
 
 identity = np.array([[1, 0, 0, 0, 0, 0],
-               [0, 1, 0, 0, 0, 0],
-               [0, 0, 1, 0, 0, 0],
-               [0, 0, 0, 1, 0, 0],
-               [0, 0, 0, 0, 1, 0],
-               [0, 0, 0, 0, 0, 1]])
+                    [0, 1, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0, 0],
+                    [0, 0, 0, 1, 0, 0],
+                    [0, 0, 0, 0, 1, 0],
+                    [0, 0, 0, 0, 0, 1]])
 
-print(f"Line 29 identity \n", f"{identity}", "\n", "\n")
+print("Line 29 identity \n", f"{identity}", "\n", "\n")
 print(f"Line 30 identity dot a_matrix \n", identity.dot(a_matrix), "\n", "\n")
 print(f"Line 31 a_matrix dot identity \n", a_matrix.dot(identity), "\n", "\n")
 
 rc_matrix = np.array([[0, 0, 0, 0, 0, 1],
-               [0, 0, 0, 0, 1, 0],
-               [0, 0, 0, 1, 0, 0],
-               [0, 0, 1, 0, 0, 0],
-               [0, 1, 0, 0, 0, 0],
-               [1, 0, 0, 0, 0, 0]])
+                     [0, 0, 0, 0, 1, 0],
+                     [0, 0, 0, 1, 0, 0],
+                     [0, 0, 1, 0, 0, 0],
+                     [0, 1, 0, 0, 0, 0],
+                     [1, 0, 0, 0, 0, 0]])
 
 print(f"Line 40 rc_matrix dot a_matrix \n", rc_matrix.dot(a_matrix), "\n", "\n")
 print(f"Line  41 a_matrix dot rc_matrix \n", a_matrix.dot(rc_matrix), "\n", "\n")
-"""
+
 #
-b1 = np.array([[1, 2, 3], [2, 1, 4], [3, 2, 1]])
-b2 = np.array([[3, 4, 5], [4, 1, 2], [1, 1, 1]])
-print(f"Line 44 b1 \n", f"{b1}", "\n", "\n")
-print(f"Line 44 b2 \n", f"{b2}", "\n", "\n")
-print(b1.dot(b2))
+# b1 = np.array([[1, 2, 3], [2, 1, 4], [3, 2, 1]])
+# b2 = np.array([[3, 4, 5], [4, 1, 2], [1, 1, 1]])
+# print(f"Line 44 b1 \n", f"{b1}", "\n", "\n")
+# print(f"Line 44 b2 \n", f"{b2}", "\n", "\n")
+# print(b1.dot(b2))
 
 # g1 = np.array([1, 2, 3])
 # g2 = np.array([[3], [4], [5]])
